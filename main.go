b938c8ddc49d4844101cf8c76d34110ea87c4cc8
efekanbicermac <efekan.bicer8@gmail.com>
2023-05-13 17:13:35 +0300

    Add db migration

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	admin_models "blogproject/admin/models"
 	"blogproject/config"
 	"net/http"
 )
 
 func main() {
-
+	admin_models.Post{}.Migrate()
 	http.ListenAndServe(":8080", config.Routes())
 }
